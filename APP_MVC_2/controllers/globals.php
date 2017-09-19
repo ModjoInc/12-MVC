0903d2b30e86562008159683999bd5650f31876b
ModjoInc <habibem@gmail.com>
2017-09-19 19:29:48 +0200

    modif template APP_MVC_2

--- APP_MVC_2/controllers/globals.php
+++ APP_MVC_2/controllers/globals.php
@@ -2,4 +2,5 @@
 define('CONTROLLERS', './controllers/');
 define('VIEWS', './views/');
 define('MODELS', './models/');
+$URL = $_SERVER['REQUEST_URI'];
 ?>
--- APP_MVC_2/index.php
+++ APP_MVC_2/index.php
@@ -3,10 +3,21 @@ require 'controllers/globals.php';
 
 include CONTROLLERS . 'errors.php';
 
+if (isset($URL) && $URL != 'home')
+{
+  include VIEWS . 'header.html';
+  include VIEWS . 'subheader.html';
+  include VIEWS . 'menu.html';
+  include CONTROLLERS . 'routeur.php';
+  include VIEWS . 'footer.html';
+}
+else
+{
+  include VIEWS . 'header.html';
+  include VIEWS . 'menu.html';
+  include CONTROLLERS . 'routeur.php';
+  include VIEWS . 'footer.html';
+}
 
-include VIEWS . 'header.html';
-include VIEWS . 'menu.html';
-include CONTROLLERS . 'routeur.php';
-include VIEWS . 'footer.html';
 
  ?>
